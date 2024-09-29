cd9c0d2e48b0535743df6c7169730d5020909858
Uff20xd <donut121008@gmail.com>
2024-09-29 15:43:18 +0200

    added text hopping simulation

--- lua/NeoHop.lua
+++ lua/NeoHop.lua
@@ -1,11 +1,34 @@
 print("Plugin in lua läuft")
 
-local function showtable(table)
-  for keys, values in pairs(table) do
-    print(keys, values)
+Pos1 = 1
+Pos2 = 2
+AlphabetSmall = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
+AlphabetLarge = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"}
+
+local function Spaceless(str)
+  return string.gsub(str, "%s+", "")
+end
+
+local funnyText = Spaceless("I SUCK big balls")
+
+local function IoV(table, value)
+  local found = false 
+  for i, v in pairs(table) do
+    if v == value then
+      return i
+    end
   end
 end
-local Test = {"Yes", password="no","Balls"}
-showtable(Test)
 
-
+local function SimulateHop(text)
+  while #text > Pos1 and #text > Pos2 do
+    print(string.sub(text,Pos1,Pos1))
+    if IoV(AlphabetSmall,string.sub(text,Pos1,Pos1)) == nil then
+      Pos1 = Pos1 + IoV(AlphabetLarge,string.sub(text,Pos1,Pos1))
+    else
+      Pos1 = Pos1 + IoV(AlphabetSmall,string.sub(text,Pos1,Pos1))
+    end
+  end
+end
+print(IoV(AlphabetSmall,"b"))
+SimulateHop(funnyText)
