8e1771c7ee6de0daa1fa7bf356a275fb2210d331
Uff20xd <donut121008@gmail.com>
2024-10-02 11:08:05 +0000

    ballssa

--- lua/config/preferences.lua
+++ lua/config/preferences.lua
@@ -1,6 +1,6 @@
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set noexpandtab")
-vim.cmd("set tabstop = 4")
-vim.cmd("set shiftwidth = 4")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
 
