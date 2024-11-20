487eaf98bac8ec60e269a070b30d100438d0a078
uff20xd <donut121008@gmail.com>
2024-11-20 15:24:41 +0100

    yay

--- lua/plugins/Colors.lua
+++ lua/plugins/Colors.lua
@@ -1,2 +1,8 @@
-lol = {}
-return lol
+return {
+    "tiagovla/tokyodark.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.cmd("colorscheme tokyodark")
+    end,
+}
