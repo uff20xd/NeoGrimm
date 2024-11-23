ed921dedae632f422c01ed48f63ee2eb7993056a
uff20xd <donut121008@gmail.com>
2024-11-23 14:07:57 +0100

    balls

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -10,8 +10,6 @@ return  {
             -- Autocompletion
             {'hrsh7th/nvim-cmp'},         -- Required
             {'hrsh7th/cmp-nvim-lsp'},     -- Required
-            {'hrsh7th/cmp-buffer'},       -- Optional
-            {'hrsh7th/cmp-path'},         -- Optional
             {'saadparwaiz1/cmp_luasnip'}, -- Optional
             {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
@@ -19,4 +17,4 @@ return  {
             {'L3MON4D3/LuaSnip'},             -- Required
             {'rafamadriz/friendly-snippets'}, -- Optional
         }
-    },
+    }
