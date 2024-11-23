ed52bca20a8b8d8ff8d65c456f004fdbb8d52a73
uff20xd <donut121008@gmail.com>
2024-11-23 14:02:24 +0100

    hui

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,3 +1,17 @@
 return {
-	"neovim/nvim-lspconfig",
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        solidity = {
+          cmd = {
+            "rust-analyzer"
+          },
+          filetypes = { "rs" },
+          root_dir = require("lspconfig").util.find_git_ancestor,
+          single_file_support = true,
+        },
+      },
+    },
+  },
 }
